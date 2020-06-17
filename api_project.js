427687cc75c0a67d445694e2320b5a3612f2698d
Danielyepes <danielyeepsahumedo@hotmail.com>
2020-06-16 23:23:48 -0500

    correccion de error

--- api_project.js
+++ api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-06-17T04:14:45.983Z",
+    "time": "2020-06-17T04:23:36.721Z",
     "url": "http://apidocjs.com",
     "version": "0.23.0"
   }
